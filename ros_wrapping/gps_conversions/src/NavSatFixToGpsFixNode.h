628b798f404dc8e3c99c27d8b926f2ee37ab7a9f
Nicolas Cadart <nicolas.cadart@kitware.com>
2019-11-19 18:47:00 +0100

    Add conversion from NavSatFix to GPSFix message to support old format.

---
+++ ros_wrapping/gps_conversions/src/NavSatFixToGpsFixNode.h
@@ -0,0 +1,35 @@
+#ifndef NAV_SAT_FIX_TO_GPS_FIX_NODE_H
+#define NAV_SAT_FIX_TO_GPS_FIX_NODE_H
+
+#include <ros/ros.h>
+#include <sensor_msgs/NavSatFix.h>
+
+class NavSatFixToGpsFixNode
+{
+public:
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Constructor.
+   * @param[in] nh      Public ROS node handle, used to init publisher/subscribers.
+   * @param[in] priv_nh Private ROS node handle, used to access parameters.
+   */
+  NavSatFixToGpsFixNode(ros::NodeHandle& nh, ros::NodeHandle& priv_nh);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     GPS pose callback, converting sensor_msgs::NavSatFix to gps_common::GPSFix.
+   * @param[in] msg GPS fix with other user-defined data.
+   */
+  void NavSatFixCallback(const sensor_msgs::NavSatFix& msg);
+
+private:
+
+  // ROS publishers & subscribers
+  ros::Subscriber NavSatFixSub;
+  ros::Publisher GpsFixPub;
+
+  double GpsDip = 0.;  ///< [rad] Magnetic inclination.
+};
+
+#endif // NAV_SAT_FIX_TO_GPS_FIX_NODE_H
