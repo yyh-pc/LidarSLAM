5fe035b96af23a19454386891d3628cea12fd5a6
julia.sanchez <julia.sanchez@kitware.com>
2021-10-25 12:06:47 +0200

    [feat] Add pose graph optimizer class
    * A new Lidar state structure is created to contain log info (i.e. pose, covariance, keypoints...). It will be used to store the data in the Slam main functions so it is created in an external header file
    
    * This new structure can be used to build the pose graph and to be able to rebuild the map afterwards
    
    * This new class allows to perform a pose graph optimization :
        1. Load lidar states and link them with lidar SLAM odometry
        2. Load landmark poses
        3. Add landmark synchronized constraints to the graph between states and landmarks
        4. Optimize the graph
    
    * It is possible to use only the tags' positions (no orientations)

---
+++ slam_lib/include/LidarSlam/State.h
@@ -0,0 +1,67 @@
+//==============================================================================
+// Copyright 2019-2020 Kitware, Inc., Kitware SAS
+// Author: Sanchez Julia (Kitware SAS)
+// Creation date: 2021-11-10
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//==============================================================================
+
+#pragma once
+
+#include <Eigen/Geometry>
+#include "LidarSlam/PointCloudStorage.h"
+#include "LidarSlam/LidarPoint.h"
+
+namespace LidarSlam
+{
+
+// Structure containing one state acquired by a sensor
+// Various states can be mixed in back end process to optimize pose graph
+struct SensorState
+{
+  SensorState() = default;
+  SensorState(const Eigen::UnalignedIsometry3d& isometry, const Eigen::Matrix6d& covariance = {}, double time = 0.)
+            : Isometry(isometry),
+              Covariance(covariance),
+              Time(time)
+  {}
+
+  // Transform to go from Sensor frame to tracking frame
+  Eigen::UnalignedIsometry3d BaseToSensor = Eigen::UnalignedIsometry3d::Identity();
+  // Pose transform in world coordinates
+  Eigen::UnalignedIsometry3d Isometry = Eigen::UnalignedIsometry3d::Identity();
+  // Covariance of current pose
+  Eigen::Matrix6d Covariance;
+  // [s] Timestamp of data
+  double Time = 0.;
+  // Index to link the pose graph (G2O)
+  unsigned int Index = 0;
+};
+
+// Structure containing one state
+// States are stored in a list for further global optimization processing
+// They will be kept as long as they fit the buffer size
+struct LidarState : public SensorState
+{
+  using Point = LidarPoint;
+  using PCStoragePtr = std::shared_ptr<PointCloudStorage<Point>>;
+
+  LidarState() = default;
+  LidarState(const Eigen::UnalignedIsometry3d& isometry, const Eigen::Matrix6d& covariance = {}, double time = 0.)
+    : SensorState(isometry, covariance, time) {};
+  // Keypoints extracted at current pose, undistorted and expressed in BASE coordinates
+  std::map<Keypoint, PCStoragePtr> Keypoints;
+  bool IsKeyFrame = true;
+};
+
+} // end of LidarSlam namespace
